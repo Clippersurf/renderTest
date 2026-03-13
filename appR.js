da7ac27ef386ecdb5be8f77fa79f0d0c50b102bd
Clippersurf <clippersurf@gmail.com>
2026-03-13 15:47:47 +0100

    render test

---
+++ appR.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const appR = express();
+
+const PORT = process.env.PORT || 3000;
+
+appR.get("/", (req, res) => {
+  res.send("Hello Render !");
+});
+
+appR.get("/test", (req, res) => {
+  res.json({status: "OK"});
+});
+
+appR.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
